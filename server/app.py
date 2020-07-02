fd23f037b6607ed52da24c591314e946af46b1a7
Kavish Shah <kavishshah09@gmail.com>
2020-07-02 16:13:56 +0530

    login endpoint

--- server/app.py
+++ server/app.py
@@ -43,5 +43,31 @@ def signup():
             return jsonify({"error_message": ""})
 
 
+@app.route('/api/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        obj = json.loads(request.data)
+        username = obj['username']
+        password_input = obj['password']
+
+        cur = mysql.connection.cursor()
+
+        result = cur.execute(
+            "SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            user = cur.fetchone()
+            userID = user['id']
+            password = user['password']
+            role = user['role']
+
+            if sha256_crypt.verify(password_input, password):
+                return jsonify({"error_message": "", "role": role, "user_id": userID})
+            else:
+                return jsonify({"error_message": "Password is incorrect. Please try again"})
+        else:
+            return jsonify({"error_message": f"User with username '{username}' does not exist. Sign Up if you haven't registered"})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
