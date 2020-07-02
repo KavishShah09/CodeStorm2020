8e4fed9df05503bd4b87699a7be7a414762af059
Kavish Shah <kavishshah09@gmail.com>
2020-07-02 13:02:41 +0530

    added materialize css

--- server/app.py
+++ server/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
 import json
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+mysql = MySQL(app)
 
 
 @app.route('/ping', methods=['GET', 'POST'])
