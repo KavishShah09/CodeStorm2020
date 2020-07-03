e54f1e91a89f65fb8c2518127f77a9533bacdf72
Kavish Shah <kavishshah09@gmail.com>
2020-07-03 15:45:08 +0530

    changed placeholder values

--- src/App.js
+++ src/App.js
@@ -1,51 +1,50 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import "./index.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/home";
-import About from "./components/about";
-import MainNav from "./components/MainNav";
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
-import Items from "./components/Items";
-import { Image, Button } from "react-bootstrap";
+import React, { useEffect } from 'react'
+import './App.css'
+import './index.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/home'
+import About from './components/about'
+import MainNav from './components/MainNav'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Items from './components/Items'
 
 function App() {
-  useEffect(() => {
-    // pingBackend()
-  }, []);
+	useEffect(() => {
+		// pingBackend()
+	}, [])
 
-  // const options = {
-  // 	method: 'POST',
-  // 	body: JSON.stringify({ ping: 'pong' }),
-  // 	headers: {
-  // 		'Content-Type': 'application/json',
-  // 	},
-  // }
+	// const options = {
+	// 	method: 'POST',
+	// 	body: JSON.stringify({ ping: 'pong' }),
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 	},
+	// }
 
-  // const pingBackend = async () => {
-  // 	const response = await fetch('/ping', options)
-  // 	const data = await response.json()
-  // 	console.log(data)
-  // }
+	// const pingBackend = async () => {
+	// 	const response = await fetch('/ping', options)
+	// 	const data = await response.json()
+	// 	console.log(data)
+	// }
 
-  return (
-    <div className="App">
-      <MainNav />
+	return (
+		<div className="App">
+			<MainNav />
 
-      <div className="container-fluid">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/shop" component={Items} />
-          </Switch>
-        </Router>
-      </div>
-    </div>
-  );
+			<div className="container-fluid">
+				<Router>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/signup" component={SignUp} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/shop" component={Items} />
+					</Switch>
+				</Router>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
