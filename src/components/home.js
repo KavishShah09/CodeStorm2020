91c8cf943126079a4f53f63bec3caae4514ee3dd
Kavish Shah <kavishshah09@gmail.com>
2020-07-03 14:10:16 +0530

    minor changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,25 @@
-import React from "react";
-import "../App.css";
+import React from 'react'
+import '../App.css'
 
 function Home() {
-  return (
-    <div>
-      <div className="jumbotron">
-        <h1 className="display-4 ">
-          Welcome To <strong className="text-success">Green</strong>
-          <strong>Terminal</strong>
-        </h1>
-        <p className="lead">A place where fashion is led by environment</p>
-        <hr className="my-4" />
-        <p>Click on Sign Up to get started</p>
-        <a className="btn btn-primary btn-lg" href="/signup" role="button">
-          Sign Up
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="jumbotron">
+				<h1 className="display-4 ">
+					Welcome To{' '}
+					<strong>
+						<span className="text-success">Green</span>Terminal
+					</strong>
+				</h1>
+				<p className="lead">A place where fashion is led by environment</p>
+				<hr className="my-4" />
+				<p>Click on Sign Up to get started</p>
+				<a className="btn btn-primary btn-lg" href="/signup" role="button">
+					Sign Up
+				</a>
+			</div>
+		</div>
+	)
 }
 
-export default Home;
+export default Home
