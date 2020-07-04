1932c2ebd34a8731e80baf9acb03241b974543f3
Ishan Saksena <saksenaishan@gmail.com>
2020-07-04 13:44:51 +0530

    about page

--- src/components/about.js
+++ src/components/about.js
@@ -23,8 +23,8 @@ function About() {
               <Card.Body>
                 <Card.Title> Re-use old clothes</Card.Title>
                 <Card.Text>
-                  We try to educateconsumers about repairing and recycling their
-                  clothes and operate repairing services. We also use old
+                  We try to educate consumers about repairing and recycling
+                  their clothes and operate repairing services. We also use old
                   recyclable material to make new clothing.
                 </Card.Text>
               </Card.Body>
@@ -50,8 +50,8 @@ function About() {
               <Card.Body>
                 <Card.Title>Make energy savings</Card.Title>
                 <Card.Text>
-                  Beyond the materials stage, firms can reduce energy use in
-                  their warehouses, stores and offices. Small improvements like
+                  Beyond the materials stage, We reduce energy use in our
+                  warehouses, stores and offices. Small improvements like
                   switching to energy-efficient lightbulbs can reduce emissions
                   by 70%.
                 </Card.Text>
@@ -60,6 +60,52 @@ function About() {
           </Col>
         </Row>
         <br />
+      </div>
+      <br />
+      <h2> We have a Unique Rating System</h2>
+      <br />
+      <div className="container-fluid  rating-system d-flex justify-content-center ">
+        <Card
+          className="color-card"
+          border="dark"
+          bg="warning"
+          style={{ width: "50rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Okay</Card.Title>
+            <Card.Text>
+              Despite this being our lowest rating, with our re-use and recycle
+              methods, manufacture of these goods causes 20% less emissions than
+              a regular piece of clothing.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card border="dark" bg="warning" style={{ width: "50rem" }}>
+          <Card.Body>
+            <Card.Title>Good</Card.Title>
+            <Card.Text>
+              We use recyclable eco-friendly material in the manufacture of
+              these goods, causing 40% less emissions than regular pieces of
+              clothing.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card
+          className="rating-card"
+          border="dark"
+          bg="success"
+          style={{ width: "50rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Best</Card.Title>
+            <Card.Text>
+              We use 100% recycled material + Reused material in the manufacture
+              of these goods, Reducing the emissions caused by a whopping 60%!
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
