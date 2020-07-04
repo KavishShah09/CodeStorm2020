6b481930e3b049ffdcfa8e75d1b3e6ac6b1667ec
Ishan Saksena <saksenaishan@gmail.com>
2020-07-05 00:09:31 +0530

    review page

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,208 +1,87 @@
 import React, { useState, useEffect } from "react";
+import { Col, Row, Card, Form, Button, Toast, CardDeck } from "react-bootstrap";
 
 function Review() {
   return (
     <div>
-      <link
-        href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
-        rel="stylesheet"
-        id="bootstrap-css"
-      />
-      {/*---- Include the above in your HEAD tag --------*/}
-      <div className="container">
-        <div className="row">
-          <h2>Feedback</h2>
-          <div className="col-md-9 col-md-offset-0"></div>
-          <table width="100%" border={0}>
-            <tbody>
-              <tr>
-                <td width="77%">
-                  <div className>
-                    <form
-                      className="form-horizontal"
-                      action="send.php"
-                      method="post"
-                    >
-                      <fieldset>
-                        {/* Name input*/}
-                        <div className="form-group">
-                          <label
-                            className="col-md-3 control-label"
-                            htmlFor="name"
-                          >
-                            Full Name
-                          </label>
-                          <div className="col-md-9">
-                            <input
-                              id="name"
-                              name="name"
-                              type="text"
-                              placeholder="Your name"
-                              className="form-control"
-                            />
-                          </div>
-                        </div>
-                        {/* Email input*/}
-                        <div className="form-group">
-                          <label
-                            className="col-md-3 control-label"
-                            htmlFor="email"
-                          >
-                            Your E-mail
-                          </label>
-                          <div className="col-md-9">
-                            <input
-                              id="email"
-                              name="email"
-                              type="text"
-                              placeholder="Your email"
-                              className="form-control"
-                            />
-                          </div>
-                        </div>
-                        {/* Message body */}
-                        <div className="form-group">
-                          <label
-                            className="col-md-3 control-label"
-                            htmlFor="message"
-                          >
-                            Your message
-                          </label>
-                          <div className="col-md-9">
-                            <textarea
-                              className="form-control"
-                              id="message"
-                              name="message"
-                              placeholder="Please enter your feedback here..."
-                              rows={5}
-                              defaultValue={""}
-                            />
-                          </div>
-                        </div>
-                        {/* Rating */}
-                        <div className="form-group">
-                          <label
-                            className="col-md-3 control-label"
-                            htmlFor="message"
-                          >
-                            Your rating
-                          </label>
-                          <div className="col-md-9">
-                            <input
-                              id="input-21e"
-                              defaultValue={0}
-                              type="number"
-                              className="rating"
-                              min={0}
-                              max={5}
-                              step="0.5"
-                              data-size="xs"
-                            />
-                          </div>
-                        </div>
-                      </fieldset>
-                    </form>
-                  </div>
-                </td>
-                <td align="center" valign="top" width="23%">
-                  {/* Form actions */}
-                  <div className="form-group">
-                    <div className="col-md-12 text-center">
-                      <img
-                        src="http://www.gohacking.com/wp-content/uploads/2010/06/captcha-300x171.jpg"
-                        height="140px"
-                        width="270px"
-                      />
-                      <br />
-                      <button type="submit" className="btn btn-primary btn-md">
-                        Submit
-                      </button>
-                      <button type="reset" className="btn btn-default btn-md">
-                        Clear
-                      </button>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="container">
-          <div className="row">
-            <h2>Featured Testimonials</h2>
-            <div className="col-md-6">
-              <div className="blockquote-box clearfix">
-                <div className="square pull-left">
-                  <img
-                    src="http://placehold.it/60/8e44ad/FFF&text=B"
-                    alt=""
-                    className
-                  />
-                </div>
-                <h4>John Doe</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.
-                </p>
-              </div>
-              <div className="blockquote-box blockquote-primary clearfix">
-                <div className="square pull-left">
-                  <span className="glyphicon glyphicon-music glyphicon-lg" />
-                </div>
-                <h4>John Doe</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.{" "}
-                  <a href="http://www.jquery2dotnet.com/search/label/jquery">
-                    jquery2dotnet
-                  </a>
-                </p>
-              </div>
-              <div className="blockquote-box blockquote-success clearfix">
-                <div className="square pull-left">
-                  <span className="glyphicon glyphicon-tree-conifer glyphicon-lg" />
-                </div>
-                <h4>John Doe</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="blockquote-box blockquote-info clearfix">
-                <div className="square pull-left">
-                  <span className="glyphicon glyphicon-info-sign glyphicon-lg" />
-                </div>
-                <h4>John Doe</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.
-                </p>
-              </div>
-              <div className="blockquote-box blockquote-warning clearfix">
-                <div className="square pull-left">
-                  <span className="glyphicon glyphicon-warning-sign glyphicon-lg" />
-                </div>
-                <h4>John Doe</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.
-                </p>
-              </div>
-              <div className="blockquote-box blockquote-danger clearfix">
-                <div className="square pull-left">
-                  <span className="glyphicon glyphicon-record glyphicon-lg" />
-                </div>
-                <h4>John Doe</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <br />
+      <br />
+      <CardDeck>
+        <Card border="primary" style={{ width: "18rem" }}>
+          <Card.Header className="text-success">Good product</Card.Header>
+          <Card.Body>
+            <Card.Title>Review by Raju Rastogi</Card.Title>
+            <Card.Text>Looks great and arrived in time.</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card border="primary" style={{ width: "18rem" }}>
+          <Card.Header className="text-warning">Okay product</Card.Header>
+          <Card.Body>
+            <Card.Title>Review by Mrs. Rastogi</Card.Title>
+            <Card.Text>
+              Good product but very overpriced. Atayees ki hogayi hai Kammo,
+              Maruti 800 mangtay hain Dahej main, aray tu itna expensive cheez
+              becheja toh shaddi kaise hogi iski
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card border="primary" style={{ width: "18rem" }}>
+          <Card.Header className="text-danger">Bad product</Card.Header>
+          <Card.Body>
+            <Card.Title>Review by Walter White</Card.Title>
+            <Card.Text>I am the danger.</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+
+      <br />
+      <br />
+
+      <h3 className="text-white">Add a review</h3>
+      <br />
+      <div className="feedback-form d-flex justify-content-center text-white">
+        <Form style={{ width: "50rem" }}>
+          <Row>
+            <Col>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="text" placeholder="Enter your email" />
+              </Form.Group>
+            </Col>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label>Rating</Form.Label>
+              <Form.Control as="select">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
+            </Form.Group>
+          </Row>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Your review</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <Button variant="dark" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
   );
--- src/components/about.js
+++ src/components/about.js
@@ -1,99 +1,115 @@
-import React from 'react'
-import '../App.css'
-import { Col, Row, Card } from 'react-bootstrap'
+import React from "react";
+import "../App.css";
+import { Col, Row, Card } from "react-bootstrap";
 
 function About() {
-	return (
-		<div className="about">
-			<h1 className="heading">About Us</h1>
-			<p className="sub-heading">
-				GreenTerminal is a site that makes customers educated decisions before <br />
-				buying anything that may have harmed the environment in the making.
-				<br />
-				<br />
-			</p>
-			<br />
-			<h2> We have a Unique Rating System</h2>
-			<br />
-			<div className="container-fluid  rating-system d-flex justify-content-center threeway">
-				<Card className="rating-card" border="dark" bg="success" style={{ width: '50rem' }}>
-					<Card.Body>
-						<Card.Title>Best</Card.Title>
-						<Card.Text>
-							We use 100% recycled material + Reused material in the manufacture of these goods, Reducing the emissions
-							caused by a whopping 60%!
-						</Card.Text>
-					</Card.Body>
-				</Card>
-				<br />
-				<Card border="dark" bg="warning" style={{ width: '50rem' }}>
-					<Card.Body>
-						<Card.Title>Good</Card.Title>
-						<Card.Text>
-							We use recyclable eco-friendly material in the manufacture of these goods, causing 40% less emissions than
-							regular pieces of clothing.
-						</Card.Text>
-					</Card.Body>
-				</Card>
-				<br />
-				<Card className="color-card" border="dark" bg="warning" style={{ width: '50rem' }}>
-					<Card.Body>
-						<Card.Title>Okay</Card.Title>
-						<Card.Text>
-							Despite this being our lowest rating, with our re-use and recycle methods, manufacture of these goods
-							causes 20% less emissions than a regular piece of clothing.
-						</Card.Text>
-					</Card.Body>
-				</Card>
-			</div>
-			<br />
-			<br />
-			<h2> We Reduce the negative effects of fashion in three ways</h2>
-			<br />
-			<div className="container-fluid threeway">
-				<Row>
-					<Col>
-						<Card border="success" bg="dark" style={{ width: '18rem' }}>
-							<Card.Header>Recycle</Card.Header>
-							<Card.Body>
-								<Card.Title> Re-use old clothes</Card.Title>
-								<Card.Text>
-									We try to educate consumers about repairing and recycling their clothes and operate repairing
-									services. We also use old recyclable material to make new clothing.
-								</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
-						<Card border="success" bg="success" style={{ width: '18rem' }}>
-							<Card.Header>Replace</Card.Header>
-							<Card.Body>
-								<Card.Title> Switch raw materials</Card.Title>
-								<Card.Text>
-									{' '}
-									A switch from virgin polyester to recycled material – made by mechanically or chemically breaking down
-									plastic drinks bottles – can reduce the carbon footprint of polyester by 40%.
-								</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
-						<Card border="success" bg="dark" style={{ width: '18rem' }}>
-							<Card.Header>Reduce</Card.Header>
-							<Card.Body>
-								<Card.Title>Make energy savings</Card.Title>
-								<Card.Text>
-									Beyond the materials stage, We reduce energy use in our warehouses, stores and offices. Small
-									improvements like switching to energy-efficient lightbulbs can reduce emissions by 70%.
-								</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-				</Row>
-				<br />
-			</div>
-		</div>
-	)
+  return (
+    <div className="about">
+      <h1 className="heading">About Us</h1>
+      <p className="sub-heading">
+        GreenTerminal is a site that makes customers educated decisions before{" "}
+        <br />
+        buying anything that may have harmed the environment in the making.
+        <br />
+      </p>
+      <br />
+      <h2> We have a Unique Rating System</h2>
+      <br />
+      <div className="container-fluid  rating-system d-flex justify-content-center threeway">
+        <Card
+          className="rating-card"
+          border="dark"
+          bg="success"
+          style={{ width: "50rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Best</Card.Title>
+            <Card.Text>
+              We use 100% recycled material + Reused material in the manufacture
+              of these goods, Reducing the emissions caused by a whopping 60%!
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card border="dark" bg="warning" style={{ width: "50rem" }}>
+          <Card.Body>
+            <Card.Title>Good</Card.Title>
+            <Card.Text>
+              We use recyclable eco-friendly material in the manufacture of
+              these goods, causing 40% less emissions than regular pieces of
+              clothing.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card
+          className="color-card"
+          border="dark"
+          bg="warning"
+          style={{ width: "50rem" }}
+        >
+          <Card.Body>
+            <Card.Title>Okay</Card.Title>
+            <Card.Text>
+              Despite this being our lowest rating, with our re-use and recycle
+              methods, manufacture of these goods causes 20% less emissions than
+              a regular piece of clothing.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <br />
+      <br />
+      <h2> We Reduce the negative effects of fashion in three ways</h2>
+      <br />
+      <div className="container-fluid threeway">
+        <Row>
+          <Col>
+            <Card border="success" bg="dark" style={{ width: "18rem" }}>
+              <Card.Header>Recycle</Card.Header>
+              <Card.Body>
+                <Card.Title> Re-use old clothes</Card.Title>
+                <Card.Text>
+                  We try to educate consumers about repairing and recycling
+                  their clothes and operate repairing services. We also use old
+                  recyclable material to make new clothing.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card border="success" bg="success" style={{ width: "18rem" }}>
+              <Card.Header>Replace</Card.Header>
+              <Card.Body>
+                <Card.Title> Switch raw materials</Card.Title>
+                <Card.Text>
+                  {" "}
+                  A switch from virgin polyester to recycled material – made by
+                  mechanically or chemically breaking down plastic drinks
+                  bottles – can reduce the carbon footprint of polyester by 40%.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card border="success" bg="dark" style={{ width: "18rem" }}>
+              <Card.Header>Reduce</Card.Header>
+              <Card.Body>
+                <Card.Title>Make energy savings</Card.Title>
+                <Card.Text>
+                  Beyond the materials stage, We reduce energy use in our
+                  warehouses, stores and offices. Small improvements like
+                  switching to energy-efficient lightbulbs can reduce emissions
+                  by 70%.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <br />
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
